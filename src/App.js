c2dfe7904f8641be47681a72b29ba8f0cff1a59b
Zhandos <saparbayevzh03@gmail.com>
2022-06-13 12:04:22 +0600

    react home work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+      <div className='img1 image'>HEY</div>
+      <div className='img2 image'>LET'S</div>
+      <div className='img3 image'>GIVE</div>
+      <div className='img4 image'>ALL</div>
+      <div className='img5 image'>YOU CAN</div>
+    </div> 
   );
 }
 
